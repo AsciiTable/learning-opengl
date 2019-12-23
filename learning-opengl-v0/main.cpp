bec8567291b39156762b75cac9628c31d99d0c4e
Jessica Wei <jessicawei9@gmail.com>
2019-12-22 22:38:16 -0800

    init notetaking

--- learning-opengl-v0/main.cpp
+++ learning-opengl-v0/main.cpp
@@ -1,1 +1,16 @@
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+	// initialize GLFW using glfwInit
+	glfwInit();
+
+	// configure GLFW using glfwWindowHint
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// allows us to access a smaller subset of OpenGL features
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // REQUIRED FOR Mac OS X FOR THIS TO WORK
+
+	return 0;
+}
