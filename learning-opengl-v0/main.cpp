135c577631c17127eac0e8384224d6ee10f59fa9
Jessica Wei <jessicawei9@gmail.com>
2019-12-22 22:41:46 -0800

    create window

--- learning-opengl-v0/main.cpp
+++ learning-opengl-v0/main.cpp
@@ -1,5 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main() {
 	// initialize GLFW using glfwInit
@@ -11,6 +12,15 @@ int main() {
 	// allows us to access a smaller subset of OpenGL features
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // REQUIRED FOR Mac OS X FOR THIS TO WORK
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
 
 	return 0;
 }
