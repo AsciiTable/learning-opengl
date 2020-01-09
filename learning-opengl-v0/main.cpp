808d87c5559c730e001d929de5a1a00062fbdd09
Jessica Wei <jessicawei9@gmail.com>
2020-01-08 22:32:58 -0800

    window up!

--- learning-opengl-v0/main.cpp
+++ learning-opengl-v0/main.cpp
@@ -2,6 +2,10 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+// declare functions
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
@@ -18,20 +22,41 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // REQUIRED FOR Mac OS X FOR THIS TO WORK
 #endif
 
+	// creates window
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-	// calls the framebuffer resize funct
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-	else
-		std::cout << "Success!";
+
 	glfwMakeContextCurrent(window);
+	// calls the framebuffer resize funct
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// stop if OpenGL function pointers are not loaded by GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// render loop
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+		// see what keys were pressed/released, mouse movement, etc..
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
 	return 0;
+}
+
+// process all input; query GLFW if keys are pressed/released this frame and act
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
 
 // Allows for window resizing
