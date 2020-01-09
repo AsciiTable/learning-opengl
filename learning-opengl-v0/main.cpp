742cddb4f8cdefb7df5d2dddefa040282d646d43
Jessica Wei <jessicawei9@gmail.com>
2020-01-08 20:42:13 -0800

    continuing with screen

--- learning-opengl-v0/main.cpp
+++ learning-opengl-v0/main.cpp
@@ -2,18 +2,25 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
 int main() {
 	// initialize GLFW using glfwInit
 	glfwInit();
-
 	// configure GLFW using glfwWindowHint
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	// allows us to access a smaller subset of OpenGL features
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // REQUIRED FOR Mac OS X FOR THIS TO WORK
+	
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // REQUIRED FOR Mac OS X FOR THIS TO WORK
+#endif
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	// calls the framebuffer resize funct
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -25,4 +32,9 @@ int main() {
 	glfwMakeContextCurrent(window);
 
 	return 0;
+}
+
+// Allows for window resizing
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
 }
