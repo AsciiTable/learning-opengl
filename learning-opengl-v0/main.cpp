1eef072cdc8f80af05e81b5dc67972ad179d4621
Jessica Wei <jessicawei9@gmail.com>
2020-01-12 17:21:21 -0800

    still at it with the triangles..

--- learning-opengl-v0/main.cpp
+++ learning-opengl-v0/main.cpp
@@ -15,6 +15,13 @@ float verticies[] = {
 	0.0f, 0.5f, 0.0f
 };
 
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
 int main() {
 	// initialize GLFW using glfwInit
 	glfwInit();
