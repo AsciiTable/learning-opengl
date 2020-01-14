4d974bca15e62470427a3c0949b9b6672606d92d
Jessica Wei <jessicawei9@gmail.com>
2020-01-13 20:20:26 -0800

    still triangles..

--- learning-opengl-v0/main.cpp
+++ learning-opengl-v0/main.cpp
@@ -22,6 +22,13 @@ const char* vertexShaderSource = "#version 330 core\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 	"}\0";
 
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
 int main() {
 	// initialize GLFW using glfwInit
 	glfwInit();
