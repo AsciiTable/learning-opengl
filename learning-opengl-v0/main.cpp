046334ab3f42ac626f17d6f531337ff91128dbb0
Jessica Wei <jessicawei9@gmail.com>
2020-01-15 20:59:00 -0800

    triangles...

--- learning-opengl-v0/main.cpp
+++ learning-opengl-v0/main.cpp
@@ -61,6 +61,20 @@ int main() {
 		return -1;
 	}
 
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+// ------------------------------------------------------------------
+	float vertices[] = {
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,  // first Triangle
+		1, 2, 3   // second Triangle
+	};
+
+
 	// render loop
 	while (!glfwWindowShouldClose(window)) {
 		// take input at every frame
